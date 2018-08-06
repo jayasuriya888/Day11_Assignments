15aa903b6c44d1a149193cb35be74056386d452f
Nexwave <Nexwave@DESKTOP-TDF6V7V>
2018-08-06 08:45:33 +0530

    Add project files.

---
+++ JS1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+function validate()
+{
+    var name = document.getElementById("txtname").value;
+    if (name == "") {
+        alert("Name cannot be blank.");
+        return;
+    }
+}
